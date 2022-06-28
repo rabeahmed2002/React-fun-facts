9ef684944e3d077ec72c717064383fc8eadf4dab
rabeahmed2002 <89692415+rabeahmed2002@users.noreply.github.com>
2022-06-28 11:52:26 -0700

    Dark mode
    Added dark mode feature using state and props

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import './style.css';
 import Navbar from "./Navbar"
 import Main from "./Main"
+import Graylogo from "./Graylogo"
+import React from 'react';
 
 export default function App() {
+  const [darkmode, setDarkmode]=React.useState(true)
+  
+  function handleChange() {
+    setDarkmode(prevState=>!prevState)
+    console.log(darkmode)
+  }
+
   return (
       <div className="container">
-          <Navbar />
-          <Main />
+          <Navbar darkmode={darkmode} handleChange={handleChange}/>
+          <Main darkmode={darkmode} />
       </div>
   )
 }
