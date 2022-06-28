04eab834ce43cb4681b2086e14ec7f315dd32d6c
rabeahmed2002 <89692415+rabeahmed2002@users.noreply.github.com>
2022-06-28 11:57:10 -0700

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -1,11 +1,17 @@
 import reactLogo from "../src/images/react-logo.png"
 
-export default function Navbar() {
+export default function Navbar(props) {
     return(
-        <nav>
+        <nav className={props.darkmode ? "" : "light"}>
             <img src={reactLogo} className="nav--icon" />
             <h3 className="nav--logo_text">React Facts</h3>
             <h4 className="nav--title">React - Project 1</h4>
+            <p>Light</p>
+            <div 
+                className="toggler-slider"
+                onClick={props.handleChange}
+            ></div>
+            <p>Dark</p>
         </nav>  
     )
 }
